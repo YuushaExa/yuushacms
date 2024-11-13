ae18844d38cff74db512045d8f3e8a807b08f0cf
YuushaExa <61891530+YuushaExa@users.noreply.github.com>
2024-11-14 01:14:52 +0300

    Update ssg.js

--- ssg.js
+++ ssg.js
@@ -223,7 +223,7 @@ function sanitizeSlug(slug, maxLength = 50) {
     // Convert to lowercase, replace spaces and encode the slug in one go
     slug = encodeURIComponent(slug.toLowerCase())
       .replace(/%20/g, '-') // Replace encoded spaces with hyphens
-        .replace(/[^\w-]+/g, '-') // Replace invalid characters with hyphens
+ .replace(/[^\w\s-]+/g, '-') // Allow Unicode characters
         .replace(/--+/g, '-') // Replace multiple hyphens with a single hyphen
         .replace(/^-+|-+$/g, ''); // Trim hyphens from start and end
 
