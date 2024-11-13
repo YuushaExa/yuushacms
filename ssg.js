94662479c0d7edd2fc4e94dac0a1ff98086036b4
YuushaExa <61891530+YuushaExa@users.noreply.github.com>
2024-11-14 01:10:29 +0300

    Update ssg.js

--- ssg.js
+++ ssg.js
@@ -221,7 +221,12 @@ async function fetchCsv(url) {
 // Function to sanitize the slug for file names
 function sanitizeSlug(slug, maxLength = 50) {
     // Convert to lowercase, replace spaces and encode the slug in one go
-    slug = encodeURIComponent(slug.toLowerCase());
+    slug = encodeURIComponent(slug.toLowerCase())
+      .replace(/%20/g, '-') // Replace encoded spaces with hyphens
+        .replace(/[^\w-]+/g, '-') // Replace invalid characters with hyphens
+        .replace(/--+/g, '-') // Replace multiple hyphens with a single hyphen
+        .replace(/^-+|-+$/g, ''); // Trim hyphens from start and end
+
     return slug.length > maxLength ? slug.substring(0, maxLength) : slug;
 }
 
