db5437549433982df1acf0cad3610ddfaf292e25
YuushaExa <61891530+YuushaExa@users.noreply.github.com>
2024-12-26 04:32:38 +0300

    Update dataExtractor.js

--- dataExtractor.js
+++ dataExtractor.js
@@ -82,7 +82,6 @@ async function generateMarkdownFromCsv(data) {
 
     try {
       await fs.writeFile(markdownFilePath, markdownContent);
-      console.log(`Created: ${markdownFilePath}`);
     } catch (error) {
       console.error(`Error creating Markdown file: ${markdownFilePath}, Error: ${error.message}`);
     }
@@ -112,7 +111,6 @@ async function generateMarkdownFromJson(data) {
 
     try {
       await fs.writeFile(markdownFilePath, markdownContent);
-      console.log(`Created: ${markdownFilePath}`);
     } catch (error) {
       console.error(`Error creating Markdown file: ${markdownFilePath}, Error: ${error.message}`);
     }
